4b2e068eb99e1026a1cbd25ac44dd496a8e57edb
Alexander Moreno <alexander.f.moreno@gmail.com>
2015-06-09 12:18:56 +0200

    have the gradient descent working, not sure it's right

--- lfvbae.py
+++ lfvbae.py
@@ -2,12 +2,12 @@ import numpy as np
 import theano as th
 import theano.tensor as T
 
-"""This class implements an auto-encoder with Variational Bayes"""
 
 class VA:
-    def __init__(self, dimX, dimTheta, n, batch_size, L, learning_rate = 0.01):
+    def __init__(self, dimX, dimTheta, m, n, batch_size, L, learning_rate = 0.01):
         self.dimX = dimX
         self.dimTheta = dimTheta
+        self.m = m
         self.n = n
         self.L = L
         self.learning_rate = learning_rate
@@ -24,6 +24,12 @@ class VA:
         lambd = np.random.normal(0, 1, (self.dimX, 1))
         self.params = [mu, sigma, lambd]
         self.h = [0.01] * len(self.params)
+
+    def initH(self, batch):
+        totalGradients = self.getGradients(batch)
+        #why are we initializing h with the squared gradients for each param?
+        for i in range(len(totalGradients)):
+            self.h[i] += totalGradients[i]*totalGradients[i]
         
     def createGradientFunctions(self):
         #create
@@ -36,27 +42,47 @@ class VA:
         #these are for testing
         self.negKL = th.function([mu, sigma], negKL)
         self.f = th.function([theta, u], f)
-        #the log-likelihood depends on f and lambda
         
-        #need to setup log-likelihood so dependent on all datapoints
+        #the log-likelihood depends on f and lambda
         #the reason we can divide like this is because we assume p(x|f) isotropic
-        #logLike = T.sum(-(0.5 * np.log(2 * np.pi) + T.log(lambd)) - 0.5 * ((x - f) / lambd)**2)
-        #self.logLike = th.function([x, mu, sigma, lambd, u, v], logLike)
-      
-        #m = T.shape(X).eval()[1] 
-        logLike = 0
         logLike = T.sum(-(0.5 * np.log(2 * np.pi) + T.log(lambd)) - 0.5 * ((X - f) / lambd)**2)
-        #+ T.sum(0.5 * ((X-f.reshape((-1, 1)))/lambd.reshape((-1, 1)))**2)
-        #T.sum((X.T-f).T)
-        #- T.sum(0.5 * ((X.T-f).T/lambd)**2)))
         self.logLike = th.function([X, mu, sigma, lambd, u, v], logLike)
 
-        '''
         logp = negKL + logLike
 
         gradvariables = [mu, sigma, lambd]
 
+        derivatives = T.grad(logp,gradvariables)
+        derivatives.append(logp)
+
         self.gradientfunction = th.function(gradvariables + [X, u, v], derivatives, on_unused_input='ignore')
         self.lowerboundfunction = th.function(gradvariables + [X, u, v], logp, on_unused_input='ignore')
-        '''
 
+    def iterate(self, data):
+        '''''Main method, slices data in minibatches and performs an iteration'''''
+        [N, dimX] = data.shape
+        totalGradients = self.getGradients(data)
+        self.updateParams(totalGradients,N,data.shape[0])
+
+    def getGradients(self, batch):
+        totalGradients = [0] * len(self.params)
+        #in our case, we only use a single sample
+        v = np.random.normal(0, 1,[self.dimTheta,1]) 
+        u = np.random.normal(0, 1,[self.dimTheta/2,1])
+        gradients = self.gradientfunction(*(self.params),X=batch,u=u,v=v)
+        self.lowerbound += gradients[-1]
+        for i in xrange(len(self.params)):
+            totalGradients[i] += gradients[i]
+        print totalGradients
+        return totalGradients
+
+    def updateParams(self, totalGradients,N,current_batch_size):
+        """Update the parameters, taking into account AdaGrad and a prior"""
+        for i in xrange(len(self.params)):
+            self.h[i] += totalGradients[i]*totalGradients[i]
+            if i < 5 or (i < 6 and len(self.params) == 12):
+                prior = 0.5 * self.params[i]
+            else:
+                prior = 0
+
+            self.params[i] += self.learning_rate/np.sqrt(self.h[i]) * (totalGradients[i] - prior*(current_batch_size/N))
--- test.py
+++ test.py
@@ -1,11 +1,14 @@
 import lfvbae
 import theano as th
+import numpy as np
 
 #make univariate data, univariate param
-encoder = lfvbae.VA(1, 2, 1, 10, 1)
+encoder = lfvbae.VA(1, 2, 2, 1, 10, 1)
 
 encoder.createGradientFunctions()
 
+#The following test the lower bound for training data
+'''
 #negKL between standard normal and standard normal should be 0
 print "this should be 0"
 print encoder.negKL([[0]], [[1]])
@@ -15,5 +18,11 @@ print "this should be 5"
 print encoder.f([[2], [2]], [[1]])
 
 #[X, mu, sigma, lambd, u, v]
-#should be approximately -2.92
+print "this should be approximately -5.84"
 print encoder.logLike([[0, 0]], [[0], [0]], [[1], [1]], [[1]], [[1]], [[1], [1]])
+'''
+X = np.array([[1, 1, 2, 5, 1]])
+encoder.initParams()
+encoder.initH(X)
+encoder.iterate(X)
+
