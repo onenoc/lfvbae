about to fix the variance issue

exponentialProblem/exponentialProblem.py
@@ -6,34 +6,42 @@ from scipy import misc
 import math
 from matplotlib import pyplot as plt
 import pdb
-
+all_gradients = []
 M=15
 Sy=1
 e_method = 0
 e_val = 0.5
 def iterate(params,i,m,v):
-    S = 50
-    simulations = 50
+    S = 20
+    simulations = 20
     b_1 = 0.9
     b_2 = 0.999
     e = 10e-8
     U1=np.random.normal(0,1,S)
-    U2=np.random.uniform(0,1,S*50)
+    U2=np.random.uniform(0,1,S)
     sn=np.random.uniform(0,1,simulations*M)
     grad_lower_bound = grad(lower_bound)
     g = grad_lower_bound(params,U1,U2,sn,i)
     LB = lower_bound(params,U1,U2,sn,i)
-    #all_gradients.append(g)
+    all_gradients.append(g)
     m = b_1*m+(1-b_1)*g
     v = b_2*v+(1-b_2)*(g**2)
     m_h = m/(1-(b_1**(i+1)))
     v_h = v/(1-(b_2**(i+1)))
     #a = 0.25
-    a = 5*(S**(1./2))*1e-2
-    a = 5*(S)*1e-2
+    a = 5*(S**(1./4))*1e-2
+    #a = 5*(S)*1e-2
     params = params+a*m_h/(np.sqrt(v_h)+e)
-    #params = params+1/(10+i)*g
+    #params = params+1./(10+i)*g
     return params,m,v,LB
+
+def gradient_lower_bound(params,num_samples,num_simulations):
+    U1=np.random.normal(0,1,num_samples)
+    U2=np.random.uniform(0,1,num_samples)
+    sn=np.random.uniform(0,1,num_simulations*M)
+    grad_lower_bound = grad(lower_bound)
+    g = grad_lower_bound(params,U1,U2,sn,i)
+    return g
 
 def lower_bound(params,U1,U2,v,i):
     E = expectation(params,U1,v,i)
@@ -44,10 +52,10 @@ def lower_bound(params,U1,U2,v,i):
 def expectation(params,U1,v,i):
     theta = generate_lognormal(params,U1)
     E=0
-    #for j in range(len(theta)):
-    #    E+=abc_log_likelihood(theta[j],j,v,i)
-    E = np.log(likelihood(theta))
-    return  np.mean(E)
+    for j in range(len(theta)):
+        E+=abc_log_likelihood(theta[j],j,v,i)
+    #E = np.log(likelihood(theta))
+    #return  np.mean(E)
     return E/len(theta)
 
 def likelihood(theta):
@@ -94,7 +102,7 @@ def log_abc_kernel(x,i,std):
 #Correct
 def generate_lognormal(params,u):
     mu = params[0]
-    sigma = np.exp(params[1])
+    sigma = params[1]
     Y = mu+sigma*u
     X = np.exp(Y)
     return X
@@ -102,7 +110,7 @@ def generate_lognormal(params,u):
 #Correct
 def lognormal_pdf(theta,params):
     mu=params[0]
-    sigma=np.exp(params[1])
+    sigma=params[1]
     x=theta
     return np.exp(-(np.log(x)-mu)**2/(2*(sigma**2)))/(x*sigma*np.sqrt(2*np.pi))
 
@@ -130,9 +138,16 @@ if __name__=='__main__':
     lower_bounds = []
     iterating=1
     i = 1
-    K = 100
+    K = 10
+    #for i in range(100):
+    #    all_gradients.append(gradient_lower_bound(params,10,1))
+    #plt.hist(all_gradients)
+    #plt.show()
+
     while iterating==1:
         params,m,v,LB = iterate(params,i,m,v)
+        if params[1]<=0:
+            params = np.random.uniform(0,1,2)
         i+=1
         #U1=np.random.uniform(0,1,30)
         #U2=np.random.uniform(0,1,50)
@@ -141,7 +156,7 @@ if __name__=='__main__':
         if len(lower_bounds)>K+1:
             lb2 = np.mean(np.array(lower_bounds[-K:]))
             lb1 = np.mean(np.array(lower_bounds[-K-1:-1]))
-            if abs(lb2-lb1)<1e-5:
+            if abs(lb2-lb1)<1e-3:
                 iterating = 0
             if i%10==0:
                 print abs(lb2-lb1)
@@ -154,8 +169,12 @@ if __name__=='__main__':
     plt.plot(x,lognormal_pdf(x,params))
     plt.plot(x,gamma.pdf(x,M+1,scale=1./(Sy*M+1)),label='true posterior')
     plt.show()
-    plt.plot(lower_bounds)
-    plt.show()
+    #plt.plot(lower_bounds)
+    #plt.ylim((-10,0))
+    #plt.show()
+    #plt.plot(all_gradients)
+    #plt.ylim((-50,50))
+    #plt.show()
     #all_gradients = np.asarray(all_gradients)
     #running_var = []
     #for i in range(1,len(all_gradients)):