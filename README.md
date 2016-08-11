6b0b69fe93670fcade9dcc5fae95a132592293e7
Alexander Moreno <onenoc@dept-143-215-34-100.vpn.gatech.edu>
2016-08-11 13:32:04 -0400

    readme

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # lfvbae
-Borrows heavily from structure of https://github.com/y0ast/Variational-Autoencoder
-Currently, the only important files are test.py and lfvbae.py
+NOTE: this code is in alpha version.
--- glmm/glmm.py
+++ glmm/glmm.py
@@ -5,7 +5,16 @@ def likelihood_i(theta,y,u):
     tau = theta[-1]
     beta = theta[:-1]
     alpha = get_alpha(tau,u)
+    w = get_w(beta,alpha)
+
+def get_w(beta,alpha):
+    
 
 def get_alpha(tau, u):
-    alpha = 
-    
+    return u*tau
+
+def bernoulli(p,y):
+    return (p**y)*((1-p)**(1-y))
+
+def get_p_ij(beta,alpha,x_ij):
+    return np.dot(beta,x_ij)+alpha_i
