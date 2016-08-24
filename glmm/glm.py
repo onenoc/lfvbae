b08b34f1badf85b31d70f7ba8c9b245687b8b461
Alexander Moreno <onenoc@lawn-143-215-103-34.lawn.gatech.edu>
2016-08-24 18:25:51 -0400

    has some bug

--- glmm/glm.py
+++ glmm/glm.py
@@ -2,11 +2,58 @@ import autograd.numpy as np
 from scipy import stats
 from autograd import grad
 
+def iterate(params,y,X,i,m,v,num_samples):
+    b_1 = 0.9
+    b_2 = 0.999
+    e = 10e-8
+    g = gradient_lower_bound(params,y,X,num_samples)
+    m = b_1*m+(1-b_1)*g
+    v = b_2*v+(1-b_2)*(g**2)
+    m_h = m/(1-(b_1**(i+1)))
+    v_h = v/(1-(b_2**(i+1)))
+    a = 5*(num_samples**(1./2))*1e-2
+    params = params+a*m_h/(np.sqrt(v_h)+e)
+    return params,m,v
+
 def generate_data(beta,num_samples):
     X = np.random.uniform(-2,2,num_samples)
     P = logistic(beta*X)
     y = np.random.binomial(1,P)
     return X,y
+
+def gradient_lower_bound(params,y,X,num_samples):
+    eps = np.random.rand(num_samples)
+    gradient_lower_bound = grad(lower_bound)
+    g = gradient_lower_bound(params,y,X,eps)
+    return g
+
+def lower_bound(params,y,X,eps):
+    E = expectation(params,y,X,eps)
+    KL = KL_via_sampling(params,eps)
+    return E-KL
+
+def expectation(params,y,X,eps):
+    beta = params[0]+np.exp(params[1])*eps
+    E = 0
+    for j in range(len(beta)):
+        for i in range(len(y)):
+            E += np.log(likelihood_i(beta[j],y[i],X[i]))
+    #    E+= np.log(likelihood(beta[j],y,X))
+    return E/len(beta)
+
+def KL_via_sampling(params,eps):
+    theta = params[0]+np.exp(params[1])*eps
+    muPrior = 0
+    sigmaPrior = 1
+    paramsPrior = np.array([muPrior,sigmaPrior])
+    E = np.log(normal_pdf(theta,params)/normal_pdf(theta,paramsPrior))
+    E = np.mean(E)
+    return E
+
+def normal_pdf(theta,params):
+    mu = params[0]
+    sigma = params[1]
+    return np.exp(-(theta-mu)**2/(2*sigma**2))/np.sqrt(2*sigma**2*np.pi)
 
 def likelihood(beta, y,X):
     likelihood = 1
@@ -29,6 +76,11 @@ def logistic(x):
 
 if __name__=='__main__':
     #create some data with beta = 2
-    X,y = generate_data(2,100)
+    params = np.array([10.,2.])
+    X,y = generate_data(5,100)
     #test likelihood for several beta values, beta = 2 should give high likelihood
-    print likelihood(2,y,X)
+    m = np.array([0.,0.])
+    v = np.array([0.,0.])
+    for i in range(100):
+        params,m,v =iterate(params,y,X,i,m,v,20)
+        print params[0], np.exp(params[1])
